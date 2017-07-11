2fcb9bf19058ca849d8d95dbbd0c484f684f4ef2
Dmytro Starosud <d.starosud@gmail.com>
2017-07-11 23:09:04 +0300

    render in Erlang

--- apps/sample/src/post.erl
+++ apps/sample/src/post.erl
@@ -6,35 +6,47 @@
 
 post_id() -> wf:to_integer(wf:q(<<"id">>)).
 
-main() -> 
-	case kvs:get(post, post_id()) of
-		{ok, Post} -> #dtl{file="post", bindings=[
-			{title, wf:html_encode(Post#post.title)}, 
-			{text, wf:html_encode(Post#post.text)},
-			{author, wf:html_encode(Post#post.author)},
-			{comments, comments()}]};
-		_ -> wf:state(status,404), "Post not found" end.
+main() ->
+  case kvs:get(post, post_id()) of
+    {ok, Post} -> #dtl{file = "post", bindings = [
+      {content, content(Post)},
+      {title, wf:html_encode(Post#post.title)}
+    ]};
+    _ -> wf:state(status, 404), "Post not found" end.
 
 comments() ->
-	case wf:user() of
-		undefined -> #link{body = "Login to add comment", url="/login"};
-		_ -> [
-				#textarea{id=comment, class=["form-control"], rows=3},
-      			#button{id=send, class=["btn", "btn-default"], body="Post comment",postback=comment,source=[comment]} 
-		] end.
-	
+  case wf:user() of
+    undefined -> #link{body = "Login to add comment", url = "/login"};
+    _ -> [
+      #textarea{id = comment, class = ["form-control"], rows = 3},
+      #button{id = send, class = ["btn", "btn-default"], body = "Post comment", postback = comment, source = [comment]}
+    ] end.
+
 event(init) ->
-	wf:reg({post, post_id()}),
-	[event({client,Comment}) || Comment <- kvs:entries(kvs:get(feed, {post, post_id()}),comment,undefined) ];
+  wf:reg({post, post_id()}),
+  [event({client, Comment}) || Comment <- kvs:entries(kvs:get(feed, {post, post_id()}), comment, undefined)];
 
 event(comment) ->
-	Comment = #comment{id=kvs:next_id("comment",1),author=wf:user(),feed_id={post, post_id()},text=wf:q(comment)},
-	kvs:add(Comment),
-	wf:send({post, post_id()}, {client, Comment});
+  Comment = #comment{id = kvs:next_id("comment", 1), author = wf:user(), feed_id = {post, post_id()}, text = wf:q(comment)},
+  kvs:add(Comment),
+  wf:send({post, post_id()}, {client, Comment});
 
 event({client, Comment}) ->
-	wf:insert_bottom(comments,
-		#blockquote{body = [
-			#p{body = wf:html_encode(Comment#comment.text)},
-			#footer{body = wf:html_encode(Comment#comment.author)}
-		]}).
+  wf:insert_bottom(comments,
+    #blockquote{body = [
+      #p{body = wf:html_encode(Comment#comment.text)},
+      #footer{body = wf:html_encode(Comment#comment.author)}
+    ]}).
+
+content(Post) -> [
+  #h1{body = [
+    wf:html_encode(Post#post.title), #br{},
+    #small{body = ["by ", wf:html_encode(Post#post.author)]}
+  ]},
+
+  #p{body = wf:html_encode(Post#post.text)},
+
+  #h3{body = "Comments"},
+  #'div'{id = comments},
+  comments()
+].
