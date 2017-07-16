4ff0d757f095b87e31ee6433440b59e0782b596f
Dmytro Starosud <d.starosud@gmail.com>
2017-07-16 13:11:12 +0300

    render in Erlang

--- apps/sample/src/post.erl
+++ apps/sample/src/post.erl
@@ -8,10 +8,7 @@ post_id() -> wf:to_integer(wf:q(<<"id">>)).
 
 main() ->
   case kvs:get(post, post_id()) of
-    {ok, Post} -> #dtl{file = "post", bindings = [
-      {content, content(Post)},
-      {title, wf:html_encode(Post#post.title)}
-    ]};
+    {ok, Post} -> html(Post);
     _ -> wf:state(status, 404), "Post not found" end.
 
 comments() ->
@@ -38,15 +35,57 @@ event({client, Comment}) ->
       #footer{body = wf:html_encode(Comment#comment.author)}
     ]}).
 
-content(Post) -> [
-  #h1{body = [
-    wf:html_encode(Post#post.title), #br{},
-    #small{body = ["by ", wf:html_encode(Post#post.author)]}
-  ]},
+content(Post) ->
+  #'div'{
+    class = container,
+    body = [
+      #h1{body = [
+        wf:html_encode(Post#post.title), #br{},
+        #small{body = ["by ", wf:html_encode(Post#post.author)]}
+      ]},
 
-  #p{body = wf:html_encode(Post#post.text)},
+      #p{body = wf:html_encode(Post#post.text)},
 
-  #h3{body = "Comments"},
-  #'div'{id = comments},
-  comments()
+      #h3{body = "Comments"},
+      #'div'{id = comments},
+      comments()
+    ]}.
+
+
+head(Title) ->
+  #head{body = [
+    #meta{charset = "utf-8"},
+    #meta{http_equiv = "X-UA-Compatible", content = "IE=edge"},
+    #meta{name = "viewport", content = "width=device-width, initial-scale=1"},
+    #title{body = Title},
+    #meta_link{
+      href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css",
+      rel = "stylesheet"},
+    #style{body = ".container { max-width: 60em; }"}
+  ]}.
+
+
+scripts() -> [
+  #script{body = nitro:script()},
+  static_scripts(),
+  #script{body = "protos = [ $bert, $client ]; N2O_start();"}
 ].
+
+
+static_scripts() -> [
+  #script{src = "/n2o/protocols/bert.js"},
+  #script{src = "/n2o/protocols/client.js"},
+  #script{src = "/n2o/protocols/nitrogen.js"},
+  #script{src = "/n2o/validation.js"},
+  #script{src = "/n2o/bullet.js"},
+  #script{src = "/n2o/utf8.js"},
+  #script{src = "/n2o/template.js"},
+  #script{src = "/n2o/n2o.js"}
+].
+
+
+html(Post) ->
+  #html{body = [
+    head(wf:html_encode(Post#post.title)),
+    #body{body = [content(Post), scripts()]}
+  ]}.
